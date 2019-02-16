ffb9be4a9cb2601a06d55be09e69344259eb3730
unknown <dglin1@gmail.com>
2019-02-15 16:28:02 -0800

    added files for package distribution

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
 
 setup(name='mlist',
-      version='1.0',
+      version='0.1',
       description='Finding Prime Numbers in Lists',
       author='Bailey Lei, Fan Wu, Daniel Lin',
       author_email='email@email.net',
       url='https://github.com/UBC-MDS/mlist_Python',
-      py_modules = ['binary_search', 'find_prime', 'flatten_list_prime'],
+      packages=['mlist',],
      )
