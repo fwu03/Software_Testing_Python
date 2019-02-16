3072b7c6ac07bc55cf300a5762f7bb5dd41342c4
unknown <dglin1@gmail.com>
2019-02-15 17:51:09 -0800

    updated package for binary search

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='mlist',
-      version='0.1',
+      version='0.1.1',
       description='Finding Prime Numbers in Lists',
       author='Bailey Lei, Fan Wu, Daniel Lin',
       author_email='email@email.net',
